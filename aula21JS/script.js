1d540aff9bb6f73fd8195efa30a86b87e516d8fe
Rafaelalves95 <rafaelalvesdesouza95@gmail.com>
2024-08-12 16:49:53 -0300

    Finalizando aula
    Exercícios de JS

---
+++ aula21JS/script.js
@@ -0,0 +1,67 @@
+//Exercício 1:
+/* let mensagem = "Bem vindo ao JavaScript";
+alert(mensagem); 
+console.log(mensagem); */
+//Exercício 2.1:
+/*
+var nome = "Rafael";
+let idade = 29;
+const cidade = "São Leopoldo";
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+*/
+
+//Exercício 2.2:
+/*
+const aluno = {
+    nome: "Rafael",
+    idade: 29,
+    curso: "FrontEnd",
+};
+
+const alunos = [aluno];
+
+console.log(alunos); 
+*/
+
+//Exercício 2.3:
+/*
+let semValor = ""
+let semValor = "null"
+*/
+
+//Exercício 3.1:
+
+/* function somar(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(somar(5, 3));
+console.log(somar(2, 4)); 
+console.log(somar(7, 7));
+
+
+//Exercício 3.2:
+
+function maiorSegundo(num1, num2) {
+    return num1 > num2;
+}
+
+console.log(maiorSegundo(10, 5));  
+console.log(maiorSegundo(3, 8));   
+console.log(maiorSegundo(7, 7));
+
+//Exercício 3.3
+
+function entredezEvinte (num) {
+    return num >= 10 && num <= 20;
+} 
+
+console.log(entredezEvinte(15));
+console.log(entredezEvinte(21));
+console.log(entredezEvinte(11));
+console.log(entredezEvinte(1));
+*/
+
+  
